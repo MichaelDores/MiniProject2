f9d6be00379b42ab7163edc33f50c2e77ca763f3
MichaelDores <Dores#7690>
2019-04-10 17:22:01 -0400

    Setup: Created new model called car and added 3 properties:Make, Model & Year

---
+++ database/migrations/2019_04_10_165300_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Car', function (Blueprint $table) {
+            $table->text('Make');
+            $table->text('Model');
+            $table->year('Year');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
